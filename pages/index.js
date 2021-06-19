d5f21f0cd731d828ae16559e9227117275073368
Đức Lê <letrongduc1999@gmail.com>
2021-06-19 13:31:43 +0700

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import React from "react";
-import '../styles/globals.css'
+import '../styles/Index.module.css'
 
 export default function Home() {
   return (
