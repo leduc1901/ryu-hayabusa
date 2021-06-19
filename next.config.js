070d18daf49477c4f38b0a67b2ee795c33950f8f
Đức Lê <letrongduc1999@gmail.com>
2021-06-19 13:13:41 +0700

    vercel

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
 module.exports = {
   reactStrictMode: true,
+  assetPrefix: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css'
 }
--- pages/index.js
+++ pages/index.js
@@ -1,62 +1,130 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import React from "react";
 
 export default function Home() {
-  const [loaded, setLoaded] = React.useState(false);
-
   return (
     <div
       style={{
-        backgroundColor: "black",
-        padding: "40px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100vw",
         height: "100vh",
-        overflow: "hidden",
-        position: "relative",
-        zIndex: "800",
+        background: "linear-gradient(20deg,rgba(222,184,33,.7),#76adb0 98%)",
       }}
     >
-      <div className="left-bar" style={{ color: "white" }}>
-        <div className={`left-bar-item`}>The</div>
-        <div className={`left-bar-item`}>Master</div>
-        <div className={`left-bar-item`}>Ninja</div>
-      </div>
-      <div className="right-bar">Ninja Gaiden</div>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+          integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
+          crossorigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
+      </Head>
       <div
-        className="center-image"
-        style={{ height: "400px", margin: "180px auto", width: "640px" }}
+        style={{
+          background:
+            "linear-gradient(20deg,#e7ab17 3%,#d3b72c 40%,#81af76 77%)",
+          position: "relative",
+          overflow: "hidden",
+          width: "1311px",
+          display: "flex",
+          height: "818px",
+        }}
       >
-        <img src="/91698.jpg" style={{ maxHeight: "100%", maxWidth: "100%" }} />
-      </div>
-      <div className="main-scroller" style={{ top: "0px" }}>
-        <div
-          style={{ height: "100vh", marginTop: "20px", position: "relative" }}
-        >
-          <div id="headline">
-            <p className="first-headline zzz">Ninja</p>
-            <p className="first-headline-2">Gaiden</p>
+        <div class="menu-bar">
+          <div class="menu-item menu-item-1" style={{ animationDelay: "0ms" }}>
+            STORIES
           </div>
-
           <div
-            style={{
-              position: "absolute",
-              bottom: "20px",
-              display: "flex",
-              flexDirection: "row",
-              left: "20%",
-            }}
+            class="menu-item menu-item-2"
+            style={{ animationDelay: "200ms", marginTop: "43px" }}
           >
-            <div className="bottom-text">Ryu Hayabusa</div>
-            <div className="bottom-text" style={{ marginLeft: "80px" }}>
-              The Dragon Ninja
-            </div>
-            <img
-              src="/weapons-japan-sword-katana-wallpaper-preview.jpg"
-              style={{ width: "120px", maxHeight: "100%", marginLeft: "400px" }}
-            />
+            SERVICE
+          </div>
+          <div
+            class="menu-item menu-item-3"
+            style={{ animationDelay: "400ms", marginTop: "50px" }}
+          >
+            SUPPORT
           </div>
         </div>
+        <div class="toolbar">
+          <div class="toolbar-line"></div>
+          <div class="toolbar-line"></div>
+        </div>
+        <div class="scroll">
+          <div class="scroll-item"></div>
+        </div>
+        <div class="float-text">
+          <p>
+            As the son of renowned ninja Joe Hayabusa,[note 1] Ryu Hayabusa
+            (whose first and last names, respectively, translate literally to
+            "dragon" and "peregrine falcon") is the wielder of the legendary
+            Dragon Sword.[5] Although he appears as a normal human, he is
+            actually a demon-human hybrid whose ancestors drew their blood from
+            the same evil deities such as the Holy Vigoor Emperor, the first
+            game's main antagonist. In the Dead or Alive series, Ryu has
+            befriended fellow ninja Hayate, and joins forces with him along with
+            Kasumi and Ayane, with whom he works to take down the corrupt
+            tournament organizer Victor Donovan.
+          </p>
+        </div>
+        <div class="car-image">
+          <img src="https://i.ibb.co/L6n8NyF/23a106bcde9f07bac8d868e8e4eba5c6.png" />
+        </div>
+        <div class="camp-image">
+          <img src="https://i.ibb.co/tckLGz1/house-PNG50.png" />
+        </div>
+        <div className="fking-scroll">
+          <div class="section-one" style={{ height: "818px" }}>
+            <div class="title-section">
+              <div class="title1">
+                <p>Electric</p>
+              </div>
+              <div class="title2">
+                <p>Adventure</p>
+              </div>
+            </div>
+            <div class="action">
+              <div class="explore">
+                <p class="explore-text">KEEP THE WORLD ADVENTUROUS FOREVER</p>
+                <div class=" button">EXPLORE</div>
+              </div>
+              <div class="action-btn">
+                <div class=" btn">
+                  <i class="fa fa-home" style={{ fontSize: "20px" }}></i>
+                </div>
+                <div class=" btn">
+                  <i class="fa fa-power-off" style={{ fontSize: "20px" }}></i>
+                </div>
+                <div class=" btn">
+                  <i class="fa fa-codiepie" style={{ fontSize: "20px" }}></i>
+                </div>
+              </div>
+              <div class="drawing">
+                <img src="https://cdn.carbuzz.com/gallery-images/2021-mercedes-amg-g63-dashboard-carbuzz-410020.jpg" />
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* <div class="section-two">
+          <div class="title">
+            <div class="title-box">
+              <p class="react-reveal"
+                style="animation-fill-mode: both; animation-duration: 600ms; animation-delay: 0ms; animation-iteration-count: 1; opacity: 1; animation-name: react-reveal-745702567171635-1;">
+                Explore the</p>
+            </div>
+            <div class="title-box">
+              <p class="react-reveal"
+                style="animation-fill-mode: both; animation-duration: 600ms; animation-delay: 200ms; animation-iteration-count: 1; opacity: 1; animation-name: react-reveal-745702567171635-1;">
+                places you love</p>
+            </div>
+          </div>
+        </div>
+        <div class="section-three"></div> */}
       </div>
     </div>
   );
