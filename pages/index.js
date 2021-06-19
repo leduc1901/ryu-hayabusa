2e3bca0f4281a6117c5cf5030b340dd25af1dbb4
Đức Lê <letrongduc1999@gmail.com>
2021-06-19 13:21:15 +0700

    fix

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
           integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
           referrerpolicy="no-referrer"
         />
       </Head>
@@ -34,78 +34,78 @@ export default function Home() {
           height: "818px",
         }}
       >
-        <div class="menu-bar">
-          <div class="menu-item menu-item-1" style={{ animationDelay: "0ms" }}>
+        <div className="menu-bar">
+          <div className="menu-item menu-item-1" style={{ animationDelay: "0ms" }}>
             STORIES
           </div>
           <div
-            class="menu-item menu-item-2"
+            className="menu-item menu-item-2"
             style={{ animationDelay: "200ms", marginTop: "43px" }}
           >
             SERVICE
           </div>
           <div
-            class="menu-item menu-item-3"
+            className="menu-item menu-item-3"
             style={{ animationDelay: "400ms", marginTop: "50px" }}
           >
             SUPPORT
           </div>
         </div>
-        <div class="toolbar">
-          <div class="toolbar-line"></div>
-          <div class="toolbar-line"></div>
+        <div className="toolbar">
+          <div className="toolbar-line"></div>
+          <div className="toolbar-line"></div>
         </div>
-        <div class="scroll">
-          <div class="scroll-item"></div>
+        <div className="scroll">
+          <div className="scroll-item"></div>
         </div>
-        <div class="float-text">
+        <div className="float-text">
           <p>
             As the son of renowned ninja Joe Hayabusa,[note 1] Ryu Hayabusa
             (whose first and last names, respectively, translate literally to
-            "dragon" and "peregrine falcon") is the wielder of the legendary
+            dragon" and peregrine falcon) is the wielder of the legendary
             Dragon Sword.[5] Although he appears as a normal human, he is
             actually a demon-human hybrid whose ancestors drew their blood from
             the same evil deities such as the Holy Vigoor Emperor, the first
-            game's main antagonist. In the Dead or Alive series, Ryu has
+            games main antagonist. In the Dead or Alive series, Ryu has
             befriended fellow ninja Hayate, and joins forces with him along with
             Kasumi and Ayane, with whom he works to take down the corrupt
             tournament organizer Victor Donovan.
           </p>
         </div>
-        <div class="car-image">
-          <img src="https://i.ibb.co/L6n8NyF/23a106bcde9f07bac8d868e8e4eba5c6.png" />
+        <div className="car-image">
+          <Image src="https://i.ibb.co/L6n8NyF/23a106bcde9f07bac8d868e8e4eba5c6.png" />
         </div>
-        <div class="camp-image">
-          <img src="https://i.ibb.co/tckLGz1/house-PNG50.png" />
+        <div className="camp-image">
+          <Image src="https://i.ibb.co/tckLGz1/house-PNG50.png" />
         </div>
         <div className="fking-scroll">
-          <div class="section-one" style={{ height: "818px" }}>
-            <div class="title-section">
-              <div class="title1">
+          <div className="section-one" style={{ height: "818px" }}>
+            <div className="title-section">
+              <div className="title1">
                 <p>Electric</p>
               </div>
-              <div class="title2">
+              <div className="title2">
                 <p>Adventure</p>
               </div>
             </div>
-            <div class="action">
-              <div class="explore">
-                <p class="explore-text">KEEP THE WORLD ADVENTUROUS FOREVER</p>
-                <div class=" button">EXPLORE</div>
+            <div className="action">
+              <div className="explore">
+                <p className="explore-text">KEEP THE WORLD ADVENTUROUS FOREVER</p>
+                <div className=" button">EXPLORE</div>
               </div>
-              <div class="action-btn">
-                <div class=" btn">
-                  <i class="fa fa-home" style={{ fontSize: "20px" }}></i>
+              <div className="action-btn">
+                <div className=" btn">
+                  <i className="fa fa-home" style={{ fontSize: "20px" }}></i>
                 </div>
-                <div class=" btn">
-                  <i class="fa fa-power-off" style={{ fontSize: "20px" }}></i>
+                <div className=" btn">
+                  <i className="fa fa-power-off" style={{ fontSize: "20px" }}></i>
                 </div>
-                <div class=" btn">
-                  <i class="fa fa-codiepie" style={{ fontSize: "20px" }}></i>
+                <div className=" btn">
+                  <i className="fa fa-codiepie" style={{ fontSize: "20px" }}></i>
                 </div>
               </div>
-              <div class="drawing">
-                <img src="https://cdn.carbuzz.com/gallery-images/2021-mercedes-amg-g63-dashboard-carbuzz-410020.jpg" />
+              <div className="drawing">
+                <Image src="https://cdn.carbuzz.com/gallery-images/2021-mercedes-amg-g63-dashboard-carbuzz-410020.jpg" />
               </div>
             </div>
           </div>
